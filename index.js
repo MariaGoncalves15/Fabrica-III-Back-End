27925cf2189b915441a90a3d1a4c85583cc77dd6
anacaroline16.github.io <caroline.cordeiro@estudante.ifro.edu.br>
2025-06-15 16:23:19 -0300

    ....

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 // Aqui serÃo colocadas todos os endpoints da API
 
+// import express from 'express';
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+
+
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerFile = require('./swagger-output.json');
+
+// const app = express();
+// app.use(express.json());
+
+// app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+
+
+// import routerEndereco from './ana/rotas/endereco.js';
+// import routerFuncionario from './ana/rotas/funcionarios.js';
+// import routerFrequencia from './maria/rotas/frequencia.js';
+// import routerFormacao from './maria/rotas/rotAformacao.js';
+// import routerMarca from './anne/rotas/marca.js';
+// import routerCliente from './sofia/rotas/cliente.js';
+// import routerEquipamentos from './anne/rotas/equipamento.js';
+// import routerExercicios from './luz/rotas/exercicios.js';
+
+// app.use('/funcionarios', routerFuncionario)
+// app.use('/endereco', routerEndereco)
+// app.use('/frequencia', routerFrequencia)
+// app.use('/formacao', routerFormacao)
+// app.use('/marca', routerMarca)
+// app.use('/cliente', routerCliente)
+// app.use('/equipamentos', routerEquipamentos)
+// app.use('/exercicios', routerExercicios)
+
+// app.listen(9000, () => {
+//     const data = new Date();
+//     console.log("Servidor  rodando em: " + data);
+// });
+
+
 import express from 'express';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+import cors from 'cors';
+
+const app = express(); 
+
+
+app.use(cors({
+    origin: 'http://localhost:3000' 
+}));
+
+
+app.use(express.json());
+
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger-output.json');
-
-const app = express();
-app.use(express.json());
-
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 
 
 import routerEndereco from './ana/rotas/endereco.js';
@@ -23,16 +69,18 @@ import routerCliente from './sofia/rotas/cliente.js';
 import routerEquipamentos from './anne/rotas/equipamento.js';
 import routerExercicios from './luz/rotas/exercicios.js';
 
-app.use('/funcionarios', routerFuncionario)
-app.use('/endereco', routerEndereco)
-app.use('/frequencia', routerFrequencia)
-app.use('/formacao', routerFormacao)
-app.use('/marca', routerMarca)
-app.use('/cliente', routerCliente)
-app.use('/equipamentos', routerEquipamentos)
-app.use('/exercicios', routerExercicios)
+
+app.use('/funcionarios', routerFuncionario);
+app.use('/endereco', routerEndereco);
+app.use('/frequencia', routerFrequencia);
+app.use('/formacao', routerFormacao);
+app.use('/marca', routerMarca);
+app.use('/cliente', routerCliente);
+app.use('/equipamentos', routerEquipamentos);
+app.use('/exercicios', routerExercicios);
+
 
 app.listen(9000, () => {
     const data = new Date();
-    console.log("Servidor  rodando em: " + data);
+    console.log("🚀 Servidor rodando em: http://localhost:9000 - " + data);
 });
